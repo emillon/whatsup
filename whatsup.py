f17063bbccc42082038738c8fd263f9e49aa4f5f
Etienne Millon <me@emillon.org>
2013-08-26 17:59:00 +0200

    Basic window

--- whatsup.py
+++ whatsup.py
@@ -4,6 +4,8 @@ import argparse
 import requests
 import netrc
 
+from gi.repository import Gtk
+
 
 class NewsblurClient:
     def __init__(self, api_root):
@@ -29,13 +31,43 @@ class MockClient:
     does not connect to the Internet.
     """
     def feeds(self):
-        return {'feeds': {}}
+        d = {'feeds': {1: {'feed_title': 'Feed 1',
+                           'nt': 2,
+                           },
+                       2: {'feed_title': 'Feed 2',
+                           'nt': 1,
+                           },
+                       }
+             }
+        return d
+
+
+class WhatsupWindow(Gtk.Window):
+
+    def __init__(self, client):
+        super().__init__()
+        self.client = client
+
+        store = Gtk.ListStore(str)
+        d = client.feeds()
+        for _, f in d['feeds'].items():
+            row = [f['feed_title']]
+            store.append(row)
+
+        view = Gtk.TreeView(store)
+        renderer = Gtk.CellRendererText()
+        column = Gtk.TreeViewColumn("Feed", renderer, text=0)
+        view.append_column(column)
+
+        self.add(view)
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--no-connect',
                         help="Don't actually connect to the API",
+                        action='store_true')
+    parser.add_argument('--debug',
                         action='store_true')
     args = parser.parse_args()
 
@@ -46,11 +78,13 @@ def main():
         c = NewsblurClient('http://api.newsblur.com')
         c.login(username, password)
     d = c.feeds()
-    for f, fd in d['feeds'].items():
-        nt = fd['nt']
-        title = fd['feed_title']
-        if nt > 0:
-            print('%3d - %s' % (nt, title))
+    if args.debug:
+        from pprint import pprint
+        pprint(d)
+    win = WhatsupWindow(c)
+    win.connect("delete-event", Gtk.main_quit)
+    win.show_all()
+    Gtk.main()
 
 
 if __name__ == '__main__':
